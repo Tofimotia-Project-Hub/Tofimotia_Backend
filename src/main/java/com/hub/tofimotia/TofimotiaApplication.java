50cd2bf9d7467c21728d5b3e976aba87308755c9
eric allibalogun <allieric28@gmail.com>
2025-12-16 20:52:09 +0100

    first commit

---
+++ src/main/java/com/hub/tofimotia/TofimotiaApplication.java
@@ -0,0 +1,13 @@
+package com.hub.tofimotia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TofimotiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TofimotiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hub/tofimotia/TofimotiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hub.tofimotia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TofimotiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
